5e4dc9d55406f17a28fedf74cbedf2402c8cc0f4
Jay Joo <jayjoo0727@gmail.com>
2025-04-29 17:56:52 -0700

    jj - added ucsbDiningCommonsMenuItemFixtures.js

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,29 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneDiningCommonsMenuItem: {
+    "id": 1,
+    "diningCommonsCode": "firstCode",
+    "name": "firstName",
+    "station": "firstStation"
+  },
+  threeDiningCommonsMenuItem: [{
+    "id": 2,
+    "diningCommonsCode": "firstCode",
+    "name": "firstName",
+    "station": "firstStation"
+  },
+  {
+    "id": 3,
+    "diningCommonsCode": "secondCode",
+    "name": "secondName",
+    "station": "secondStation"
+  },
+  {
+    "id": 4,
+    "diningCommonsCode": "thirdCode",
+    "name": "thirdName",
+    "station": "thirdStation"
+  }
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
