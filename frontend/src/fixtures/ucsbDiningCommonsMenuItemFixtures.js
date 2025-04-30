e92321c9e8ec5225fb4facef22bfd659f4eaeae6
Jay Joo <jayjoo0727@gmail.com>
2025-04-30 10:19:02 -0700

    jj - WIP fix formatting

--- frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -1,28 +1,29 @@
 const ucsbDiningCommonsMenuItemFixtures = {
   oneUCSBDiningCommonsMenuItem: {
-    "id": 1,
-    "diningCommonsCode": "firstCode",
-    "name": "firstName",
-    "station": "firstStation"
+    id: 1,
+    diningCommonsCode: "firstCode",
+    name: "firstName",
+    station: "firstStation",
   },
-  threeUCSBDiningCommonsMenuItems: [{
-    "id": 2,
-    "diningCommonsCode": "firstCode",
-    "name": "firstName",
-    "station": "firstStation"
-  },
-  {
-    "id": 3,
-    "diningCommonsCode": "secondCode",
-    "name": "secondName",
-    "station": "secondStation"
-  },
-  {
-    "id": 4,
-    "diningCommonsCode": "thirdCode",
-    "name": "thirdName",
-    "station": "thirdStation"
-  }
+  threeUCSBDiningCommonsMenuItems: [
+    {
+      id: 2,
+      diningCommonsCode: "firstCode",
+      name: "firstName",
+      station: "firstStation",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "secondCode",
+      name: "secondName",
+      station: "secondStation",
+    },
+    {
+      id: 4,
+      diningCommonsCode: "thirdCode",
+      name: "thirdName",
+      station: "thirdStation",
+    },
   ],
 };
 
--- frontend/src/main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemForm.js
+++ frontend/src/main/components/UCSBDiningCommonsMenuItems/UCSBDiningCommonsMenuItemForm.js
@@ -21,84 +21,79 @@ function UCSBDiningCommonsMenuItemForm({
 
   return (
     <Form onSubmit={handleSubmit(submitAction)}>
-        {initialContents && (
-            <Form.Group className="mb-3">
-              <Form.Label htmlFor="id">Id</Form.Label>
-              <Form.Control
-                data-testid={testIdPrefix + "-id"}
-                id="id"
-                type="text"
-                {...register("id")}
-                value={initialContents.id}
-                disabled
-              />
-            </Form.Group>
-        )}
-
+      {initialContents && (
         <Form.Group className="mb-3">
-            <Form.Label htmlFor="diningCommonsCode">
-              diningCommonsCode
-            </Form.Label>
-            <Form.Control
-              data-testid={testIdPrefix + "-diningCommonsCode"}
-              id="diningCommonsCode"
-              type="text"
-              isInvalid={Boolean(errors.diningCommonsCode)}
-              {...register("diningCommonsCode", {
-                required: "diningCommonsCode is required.",
-                maxLength: {
-                  value: 255,
-                  message: "Max length 255 characters"
-                }
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.diningCommonsCode?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="name">Name</Form.Label>
-            <Form.Control
-              data-testid={testIdPrefix + "-name"}
-              id="name"
-              type="text"
-              isInvalid={Boolean(errors.name)}
-              {...register("name", {
-                required: "Name is required.",
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.name?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3">
-            <Form.Label htmlFor="station">Station</Form.Label>
-            <Form.Control
-              data-testid={testIdPrefix + "-station"}
-              id="station"
-              type="text"
-              isInvalid={Boolean(errors.station)}
-              {...register("station", {
-                required: "Station is required.",
-              })}
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.station?.message}
-            </Form.Control.Feedback>
-          </Form.Group>
-          <Button
-            type="submit"
-            data-testid={testIdPrefix + "-submit"}
-          >
-            {buttonLabel}
-          </Button>
-          <Button
-            variant="Secondary"
-            onClick={() => navigate(-1)}
-            data-testid="UCSBDiningCommonsMenuItemForm-cancel"
-          >
-            Cancel
-          </Button>
+          <Form.Label htmlFor="id">Id</Form.Label>
+          <Form.Control
+            data-testid={testIdPrefix + "-id"}
+            id="id"
+            type="text"
+            {...register("id")}
+            value={initialContents.id}
+            disabled
+          />
+        </Form.Group>
+      )}
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="diningCommonsCode">diningCommonsCode</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-diningCommonsCode"}
+          id="diningCommonsCode"
+          type="text"
+          isInvalid={Boolean(errors.diningCommonsCode)}
+          {...register("diningCommonsCode", {
+            required: "diningCommonsCode is required.",
+            maxLength: {
+              value: 255,
+              message: "Max length 255 characters",
+            },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.diningCommonsCode?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="name">Name</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-name"}
+          id="name"
+          type="text"
+          isInvalid={Boolean(errors.name)}
+          {...register("name", {
+            required: "Name is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.name?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="station">Station</Form.Label>
+        <Form.Control
+          data-testid={testIdPrefix + "-station"}
+          id="station"
+          type="text"
+          isInvalid={Boolean(errors.station)}
+          {...register("station", {
+            required: "Station is required.",
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.station?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
+      <Button
+        variant="Secondary"
+        onClick={() => navigate(-1)}
+        data-testid="UCSBDiningCommonsMenuItemForm-cancel"
+      >
+        Cancel
+      </Button>
     </Form>
   );
 }
