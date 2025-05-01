9a2cc5924cc073f883247606e30d56ddda3073ab
Simon <simonryan@ucsb.edu>
2025-04-30 19:13:01 -0700

    Simon-MenuItemReviews-added fixtures

---
+++ frontend/src/fixtures/menuItemReviewFixtures.js
@@ -0,0 +1,39 @@
+const menuItemReviewFixtures = {
+    oneReview: {
+      "id": 1,
+      "itemId": 7,
+      "reviewerEmail": "simonryan@ucsb.edu",
+      "stars": 5,
+      "comments": "amazing",
+      "dateReviewed": "2023-01-03T00:00:00"
+    },
+    threeReviews: [
+      {
+        "id": 1,
+        "itemId": 7,
+        "reviewerEmail": "simonryan@ucsb.edu",
+        "stars": 5,
+        "comments": "amazing",
+        "dateReviewed": "2023-01-03T00:00:00"
+      },
+      {
+        "id": 2,
+        "itemId": 4,
+        "reviewerEmail": "simonryan@ucsb.edu",
+        "stars": 1,
+        "comments": "bad",
+        "dateReviewed": "2023-01-03T00:00:00"
+      },
+      {
+        "id": 3,
+        "itemId": 47,
+        "reviewerEmail": "simonryan@ucsb.edu",
+        "stars": 3,
+        "comments": "average",
+        "dateReviewed": "2023-01-03T00:00:00"
+      }
+    ],
+  };
+  
+  export { menuItemReviewFixtures };
+  
