bc80c49e8feebaa160179ecd3848ffb03bfd01d5
Arthur Wu <acwu@ucsb.edu>
2025-05-02 16:34:40 -0700

    aw - add edit page for Articles

--- frontend/src/main/components/Articles/ArticlesForm.js
+++ frontend/src/main/components/Articles/ArticlesForm.js
@@ -66,6 +66,7 @@ function ArticlesForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="url">Url</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-url"}
           id="url"
           type="text"
           isInvalid={Boolean(errors.url)}
@@ -81,6 +82,7 @@ function ArticlesForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="explanation">Explanation</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-explanation"}
           id="explanation"
           type="text"
           isInvalid={Boolean(errors.explanation)}
@@ -96,6 +98,7 @@ function ArticlesForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="email">Email</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-email"}
           id="email"
           type="text"
           isInvalid={Boolean(errors.email)}
@@ -111,6 +114,7 @@ function ArticlesForm({
       <Form.Group className="mb-3">
         <Form.Label htmlFor="dateAdded">Date Added (iso format)</Form.Label>
         <Form.Control
+          data-testid={testIdPrefix + "-dateAdded"}
           id="dateAdded"
           type="datetime-local"
           isInvalid={Boolean(errors.dateAdded)}
@@ -124,7 +128,9 @@ function ArticlesForm({
         </Form.Control.Feedback>
       </Form.Group>
 
-      <Button type="submit">{buttonLabel}</Button>
+      <Button type="submit" data-testid={testIdPrefix + "-submit"}>
+        {buttonLabel}
+      </Button>
       <Button
         variant="Secondary"
         onClick={() => navigate(-1)}
