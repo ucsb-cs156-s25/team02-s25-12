32ecc5e31b76519a1db8928ee1fe60a04619b49f
Hung Khuu <hungkhuu@ucsb.edu>
2025-05-03 16:04:33 -0700

    hk - recommendation requests edit page with testing and stories

--- frontend/src/main/pages/RecommendationRequests/RecommendationRequestsEditPage.js
+++ frontend/src/main/pages/RecommendationRequests/RecommendationRequestsEditPage.js
@@ -1,11 +1,75 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams, Navigate } from "react-router-dom";
+import RecommendationRequestForm from "main/components/RecommendationRequests/RecommendationRequestForm";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function RecommendationRequestsEditPage() {
-  // Stryker disable all : placeholder for future implementation
+export default function RecommendationRequestsEditPage({ storybook = false }) {
+  let { id } = useParams();
+
+  const {
+    data: recommendationRequest,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/recommendationrequest?id=${id}`],
+    {
+      // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+      method: "GET",
+      url: "/api/recommendationrequest",
+      params: { id },
+    },
+  );
+
+  const objectToAxiosPutParams = (rr) => ({
+    url: "/api/recommendationrequest",
+    method: "PUT",
+    params: { id: rr.id },
+    data: {
+      requesterEmail: rr.requesterEmail,
+      professorEmail: rr.professorEmail,
+      explanation: rr.explanation,
+      dateRequested: rr.dateRequested,
+      dateNeeded: rr.dateNeeded,
+      done: rr.done,
+    },
+  });
+
+  const onSuccess = (rr) => {
+    toast(
+      `RecommendationRequest Updated - id: ${rr.id} requesterEmail: ${rr.requesterEmail}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/recommendationrequest?id=${id}`],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/recommendationrequests" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit Recommendation Request</h1>
+        {recommendationRequest && (
+          <RecommendationRequestForm
+            submitAction={onSubmit}
+            buttonLabel="Update"
+            initialContents={recommendationRequest}
+          />
+        )}
       </div>
     </BasicLayout>
   );
---
+++ frontend/src/stories/pages/RecommendationRequests/RecommendationRequestsEditPage.stories.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+import { recommendationRequestFixtures } from "fixtures/recommendationRequestFixtures";
+import { http, HttpResponse } from "msw";
+
+import RecommendationRequestsEditPage from "main/pages/RecommendationRequests/RecommendationRequestsEditPage";
+
+export default {
+  title: "pages/RecommendationRequests/RecommendationRequestsEditPage",
+  component: RecommendationRequestsEditPage,
+};
+
+const Template = () => <RecommendationRequestsEditPage storybook={true} />;
+
+export const Default = Template.bind({});
+Default.parameters = {
+  msw: [
+    http.get("/api/currentUser", () =>
+      HttpResponse.json(apiCurrentUserFixtures.userOnly, { status: 200 }),
+    ),
+    http.get("/api/systemInfo", () =>
+      HttpResponse.json(systemInfoFixtures.showingNeither, { status: 200 }),
+    ),
+    http.get("/api/recommendationrequest", () =>
+      HttpResponse.json(
+        recommendationRequestFixtures.oneRecommendationRequest,
+        { status: 200 },
+      ),
+    ),
+    http.put("/api/recommendationrequest", () =>
+      HttpResponse.json({}, { status: 200 }),
+    ),
+    http.put("/api/recommendationrequest", (req) => {
+      window.alert("PUT: " + req.url + " and body: " + req.body);
+      return HttpResponse.json({}, { status: 200 });
+    }),
+  ],
+};
