170025d45d8d9781403fd3940c8ab406ae4477ab
Simon <simonryan@ucsb.edu>
2025-05-03 22:30:57 -0700

    fixing edit

--- frontend/src/main/pages/MenuItemReviews/MenuItemReviewsEditPage.js
+++ frontend/src/main/pages/MenuItemReviews/MenuItemReviewsEditPage.js
@@ -1,11 +1,81 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import MenuItemReviewForm from "main/components/MenuItemReviews/MenuItemReviewForm";
+import { Navigate } from "react-router-dom";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewsEditPage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemReviewsEditPage({ storybook = false }) {
+  let { id } = useParams();
+
+  const {
+    data: menuitemreview,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/menuitemreviews?id=${id}`],
+    {
+      // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+      method: "GET",
+      url: `/api/menuitemreviews`,
+      params: {
+        id,
+      },
+    },
+  );
+
+  console.log("Fetched review data:", menuitemreview);
+
+  const objectToAxiosPutParams = (menuitemreview) => ({
+    url: "/api/menuitemreviews",
+    method: "PUT",
+    params: {
+      id: menuitemreview.id,
+    },
+    data: {
+      itemId: menuitemreview.itemid,
+      reviewerEmail: menuitemreview.revieweremail,
+      stars: menuitemreview.stars,
+      comments: menuitemreview.comments,
+      dateReviewed: menuitemreview.datereviewed,
+    },
+  });
+
+  const onSuccess = (menuitemreview) => {
+    toast(
+      `Review Updated - id: ${menuitemreview.id} reviewerEmail: ${menuitemreview.reviewerEmail}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/menuitemreviews?id=${id}`],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreviews" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit Review</h1>
+        {menuitemreview && (
+          <MenuItemReviewForm
+            submitAction={onSubmit}
+            buttonLabel={"Update"}
+            initialContents={menuitemreview}
+          />
+        )}
       </div>
     </BasicLayout>
   );
